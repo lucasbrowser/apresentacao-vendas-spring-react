6cd7c3aaccbd2d0f8c253b07bc3c60d2556d07bc
Lucas de Souza Oliveira <lucasdicas@yahoo.com.br>
2021-09-07 18:40:44 -0300

    Criando componentes ao projeto

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/lucasbrowser" target="_blank" rel="noreferrer">Lucas de Souza Oliveira</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
