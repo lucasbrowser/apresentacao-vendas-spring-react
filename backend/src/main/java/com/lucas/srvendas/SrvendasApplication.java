ce875d703353ff94658f3928fa5ee868c82e1ece
Lucas de Souza Oliveira <lucasdicas@yahoo.com.br>
2021-09-07 12:22:50 -0300

    Projeto criado

---
+++ backend/src/main/java/com/lucas/srvendas/SrvendasApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.srvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvendasApplication.class, args);
+	}
+
+}
