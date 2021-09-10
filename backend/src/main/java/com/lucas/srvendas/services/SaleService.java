c041d82510216550c65031685fd16a5b27ad044b
Lucas de Souza Oliveira <lucasdicas@yahoo.com.br>
2021-09-09 23:41:49 -0300

    Ajuste para não fazer várias buscas no banco de um chave estrangeira e evitar lock de pesquisa

--- backend/src/main/java/com/lucas/srvendas/services/SaleService.java
+++ backend/src/main/java/com/lucas/srvendas/services/SaleService.java
@@ -1,15 +1,15 @@
 package com.lucas.srvendas.services;
 
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lucas.srvendas.dto.SaleDTO;
 import com.lucas.srvendas.entities.Sale;
 import com.lucas.srvendas.repositories.SaleRepository;
+import com.lucas.srvendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -17,7 +17,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
 		Page<Sale> result = saleRepository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
